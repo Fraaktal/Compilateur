d2abef086786f85d2bca7eee24e70f18569246dd
Fraaktal <50698889+Fraaktal@users.noreply.github.com>
2020-09-02 12:28:07 +0200

    [Analyzer]

---
+++ Compilateur/Compilator/Business/Token.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Compilateur.Compilator.Business
+{
+    public class Token
+    {
+        public enum TokensType
+        {
+            Add, 
+            Sub,
+            Mult,
+            Div, 
+            Mod, 
+            Inferior, 
+            Superior, 
+            InferiorEq, 
+            SuperiorEq, 
+            Equals, 
+            Different, 
+            And, 
+            Or, 
+            Not, 
+            OpenParenthese,
+            ClosingParenthese, 
+            SemiColon, 
+            Const, 
+            Indent, 
+            If, 
+            Else, 
+            For, 
+            While, 
+            EOF
+        }
+
+        public Token()
+        {
+
+        }
+
+        public TokensType Type { get; set; }
+
+        public string StringValue { get; set; }
+
+        public int IntValue { get; set; }
+
+        public int LineNumber { get; set; }
+    }
+}
---
+++ Compilateur/Compilator/Control/LexicalAnalyzer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Compilateur.Compilator.Business;
+
+namespace Compilateur.Compilator.Control
+{
+    public class LexicalAnalyzer
+    {
+        public LexicalAnalyzer()
+        {
+
+        }
+
+        public List<Token> AnalyzeCode(string code)
+        {
+            List<Token> tokens = new List<Token>();
+
+            int index = 0;
+
+            Regex regex = new Regex("^");
+
+            while (index < code.Length)
+            {
+                char c = code.ElementAt(index);
+                Token token = new Token();
+                switch (c)
+                {
+                    case '+':
+                        token.Type = Token.TokensType.Add;
+                        break;
+                    case '-':
+                        token.Type = Token.TokensType.Sub;
+                        break;
+                    case '*':
+                        token.Type = Token.TokensType.Mult;
+                        break;
+                    case '/':
+                        token.Type = Token.TokensType.Div;
+                        break;
+                    case '%':
+                        token.Type = Token.TokensType.Mod;
+                        break;
+                    case '<':
+                        if (index < code.Length - 1 && code.ElementAt(index + 1).Equals('='))
+                        {
+                            token.Type = Token.TokensType.InferiorEq;
+                        }
+                        else
+                        {
+                            token.Type = Token.TokensType.Inferior;
+                        }
+                        break;
+                    case '>':
+                        break;
+                    case '=':
+                        break;
+                    case '!':
+                        break;
+                    case '(':
+                        break;
+                    case ')':
+                        break;
+                    case ';':
+                        break;
+                    case 'i':
+                        break;
+                    case 'f':
+                        break;
+                    case 'w':
+                        break;
+                    case 'e':
+                        break;
+                    default:
+                        break;
+                }
+
+                tokens.Add(token);
+            }
+
+            tokens.Add(new Token(){Type = Token.TokensType.EOF});
+            return tokens;
+        }
+    }
+}
