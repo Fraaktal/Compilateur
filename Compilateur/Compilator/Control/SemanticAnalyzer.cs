c289af8d215088411c25454dfcc33321da3c0134
Lorelei Augeraud <lorelei.augeraud@u-psud.fr>
2020-09-09 12:56:15 +0200

    modifs

--- Compilateur/Compilator/Control/SemanticAnalyzer.cs
+++ Compilateur/Compilator/Control/SemanticAnalyzer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Compilateur.Compilator.Business;
 
 namespace Compilateur.Compilator.Control
@@ -10,6 +11,34 @@ namespace Compilateur.Compilator.Control
 
         public SemanticAnalyzer()
         {
+            void DebutBloc()
+            {
+                Pile.Enqueue(new Dictionary<string, Symbol>());
+            }
+
+            void FinBloc()
+            {
+                Pile.Dequeue();
+            }
+
+            Symbol Declarer(String ident)
+            {
+                if (Pile.Peek().ContainsKey(ident))
+                {
+                    //Erreur
+                }
+                else
+                {
+                    Symbol s = new Symbol(ident);
+                    Pile.Peek().Add(ident, s);
+                    return s;
+                }
+            }
+
+            Symbol Accepter(String ident)
+            {
+                
+            }
 
         }
     }
